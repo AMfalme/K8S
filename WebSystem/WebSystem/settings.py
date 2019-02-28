4866e49c0a36776c31fce176edf586ffe71c6fa7
Griffin <gmfalme@duara.io>
2019-03-01 02:04:09 +0300

    user realistic host_email_user

--- WebSystem/WebSystem/settings.py
+++ WebSystem/WebSystem/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "noreply_email"
+EMAIL_HOST_USER = "griffinmfalme@gmail.com"
 EMAIL_HOST_PASSWORD = 'password'
 
 
